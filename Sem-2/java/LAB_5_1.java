d316bba275db476327df39a6feab15eca36ee7b7
DarshanSakdecha <159982286+DarshanSakdecha@users.noreply.github.com>
2024-02-20 09:46:40 +0530

    Add files via upload

---
+++ Sem-2/java/LAB_5_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Circle{
+    int radius;
+    public double getareaofCircle (double radius){
+        return Math.PI = radius = radius;
+    }
+}
+public class LAB_5_1{
+     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println(x:"enter radius of circle:");
+        double radius=sc.nextDouble();
+        Circle c =new Circle();
+        double area = c.getareaofCircle(radius);
+        System.out.println("Area:"+area);
+        sc.close();
+    }
+}
+
---
+++ Sem-2/java/lab 4/LAB_5_2.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class LAB_5_2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(s:"enter Time in hour,minute,second");
+    }
+}
